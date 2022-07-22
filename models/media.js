f79708e599cab32a7a29828ec0b5628be7e3754a
Tyler Preston <tpreston96@gmail.com>
2022-07-21 22:09:04 -0500

    update model

--- models/media.js
+++ models/media.js
@@ -5,7 +5,7 @@ const mediaSchema = new Schema(
   {
     id: { type: String, required: true },
     type: { type: String, required: true },
-    favoritedBy: { type: Schema.Types.ObjectId, ref: "Profile" },
+    favoritedBy: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
   },
   {
     timestamps: true,
